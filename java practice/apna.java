7f7d89b6f65580c1a47bd1d5b2a0ec3744f29621
Akhand Pratap Singh <113419005+akhand177@users.noreply.github.com>
2024-04-21 23:55:31 +0530

    Add files via upload

---
+++ java practice/apna.java
@@ -0,0 +1,257 @@
+/*import java.util.Scanner;
+
+public class apna {
+      public static void main ( String[] args) {
+
+        Scanner s = new Scanner (System.in);
+        System.out.println("enter length");
+        int x = s.nextInt() ;
+        System.out.println("enter breadth");
+        int y = s.nextInt();
+        if(x ==y ){
+            System.err.println("square");
+        }
+        else{
+            System.err.println("rectangle");
+        }
+
+      }
+}*/
+
+
+
+/*import java.util.Scanner;
+
+public class apna {
+
+    public static void main( String [] args){
+
+        Scanner s = new Scanner(System.in);
+        System.out.println("Input the number");
+        int input = s.nextInt();
+        if ( input > 0)
+        {
+            System.out.println("Number is pocitive ");
+        }
+        else if( input<0)
+        {
+            System.out.println( "Number is nagative");
+        }
+        else 
+        {
+            System.out.println( "number is zero");
+            
+        }
+
+    }*/
+
+
+
+    /*public class apna {
+
+        public static void main( String [] args) {
+
+            int year =2019 ;
+            if(((year%4==0) && (year%100!= 0))|| (year % 400==0)){
+                System.out.println("leap year");
+
+            }
+            else{
+                System.out.println("common year");
+            }
+        }
+    }*/
+
+
+
+
+/*import java.util.Scanner;
+
+public class apna {
+
+    public static void main (String [] args){
+
+       Scanner s = new Scanner(System.in);
+        System.out.println("enter marks");
+        int marks = s.nextInt();
+
+        if (marks<50){
+            System.err.println("fail");
+        }
+        else if ( marks>=50 && marks<60){
+            System.out.println("D grade");
+        }
+        else if( marks>=60 && marks<70){
+            System.out.println("C grade");
+        }
+        else if (marks >= 70 && marks<80){
+            System.out.println("B grade");
+        }
+        else if ( marks >=80 && marks<90){
+            System.out.println("A grade");
+        }
+        else if ( marks >=90 && marks<100 ){
+            System.out.println("A + grade ");
+        }
+        else{
+            System.out.println("invalid !");
+        }
+
+    }
+}*/
+
+
+
+/*import java.util.Scanner;
+
+public class apna {
+
+    public static void main( String [] args){
+
+        Scanner s = new Scanner(System.in );
+        System.out.println("enter the age ");
+         int age = s.nextInt() ;
+       
+        System.out.println("enter the weight");
+        int weight = s.nextInt();
+
+        if ( age >=18){
+            if (weight>=50){
+                System.out.println("you are eligible to donate blood ");
+            }else{
+                System.out.println("you are not eligible to donate blood ");
+            }
+        }else {
+            System.out.println("age must be greater than 18");
+        }
+    }
+}*/
+
+/*import java.util.Scanner;
+
+public class apna {
+ 
+    public static void main ( String [] args){
+         
+       Scanner s = new Scanner(System.in);
+       System.out.print("enter the number");
+       int number= s.nextInt();
+
+        switch (number) {
+            case 10:
+            System.out.println("10");
+            break;
+            
+            case 20:
+            System.out.println("20");
+            break;
+
+            case 30:
+            System.out.println("30");
+            break;
+
+            default:
+            System.out.println("not in 10,20,30");
+        }
+    }
+}*/
+
+
+/*public class apna {
+    public static void main ( String [] args) {
+
+        int month = 7;
+        String montString="";
+
+        switch (month) {
+            case 1: montString="1-january";
+                
+                break;
+                case 2: montString="2- february";
+
+                break;
+                case 3: montString="3- march";
+                
+                break;
+                case 4: montString="4- april";
+
+                break;
+                case 5: montString="5- may";
+
+                break;
+
+                case 6:montString="6- june";
+
+                break;
+
+                case 7:montString="7- july";
+
+                break;
+
+                case 8:montString="8- ausgust";
+
+                break;
+
+                case 9:montString="9- september";
+
+                break;
+                case 10:montString="10- october";
+
+                break;
+                case 11:montString="11- november";
+
+                break;
+                case 12: montString="12- december";
+                
+                break;
+
+                default:System.out.println("invalid month");
+        }
+
+        System.out.println(montString);
+    }
+}*/
+
+/*import java.util.Scanner;
+
+public class apna{
+
+    public static void main( String [] args){
+
+        char ch='O';    
+        switch(ch)  
+         
+       
+       {
+        case 'a':
+        System.out.println("vowel");
+        break;
+
+        case 'e':
+        System.out.println("vowel");
+        break;
+
+        case 'i':
+        System.out.println("vowel");
+        break;
+
+        case 'o':
+        System.out.println("vowel");
+        break;
+
+        case 'u':
+        System.out.println("vowel");
+        break;
+
+        default:
+        System.out.println("consonant");
+
+       }
+
+    }
+}*/
+
+
+
+
+
